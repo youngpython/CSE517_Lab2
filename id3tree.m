8f785bf0c18715b38c9b941070084d0205195d8a
Heather Berlin <heatherberlin33@gmail.com>
2015-02-20 20:04:38 -0600

    id3 w/ helper fun

--- id3tree.m
+++ id3tree.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-function T=id3tree(xTr,yTr,maxdepth,weights,current_pos,alloc)
-=======
-function T=id3tree(xTr,yTr,maxdepth,weights,current_pos)
->>>>>>> FETCH_HEAD
+function T=id3tree(xTr,yTr,maxdepth,weights)
 % function T=id3tree(xTr,yTr,maxdepth,weights)
 %
 % The maximum tree depth is defined by "maxdepth" (maxdepth=2 means one split). 
@@ -22,11 +18,7 @@ function T=id3tree(xTr,yTr,maxdepth,weights,current_pos)
 
 % Answer to question 6a - if each parent splits (roughly) binary,
 % expected value of q is, for n elements, 2^(n+1) - 1
-<<<<<<< HEAD
-disp('recursive call');
-=======
 
->>>>>>> FETCH_HEAD
 [~,n]=size(xTr);
 if nargin<4
     weights=ones(1,n)./n;
@@ -34,76 +26,39 @@ end
 if nargin<3
    maxdepth = ceil(log2(n)); %FIXME Unsure about this
 end
-if nargin<5
-    current_pos = 1;
-    disp('T refreshed');
-<<<<<<< HEAD
-end
-if nargin<6
-    T = zeros(7,n);
-    T(7,:) = 1:n;
-end
+T = zeros(7,n);
+alloc = id3treehelper(xTr,yTr,maxdepth,weights,1);
 
-if length(unique(yTr','rows')) == 1  || maxdepth == 0 %If all the y's are the same, or max depth reached
-    alloc = [yTr(1);0;0;0;0;floor(current_pos/2);current_pos];
-    
-elseif length(unique(xTr','rows')) == 1 || maxdepth == 0 %If all columns in xTr are the same, or max depth reached
-    alloc = [mode(yTr);0;0;0;0;floor(current_pos/2);current_pos];
-=======
-end
-
-if length(unique(yTr','rows')) == 1  || maxdepth == 0 %If all the y's are the same, or max depth reached
-    T = [yTr(1);0;0;0;0;floor(current_pos/2)];
-    
-elseif length(unique(xTr','rows')) == 1 || maxdepth == 0 %If all columns in xTr are the same, or max depth reached
-    T = [mode(yTr);0;0;0;0;floor(current_pos/2)];
->>>>>>> FETCH_HEAD
-else
-    [feature,cut,~] = entropysplit(xTr,yTr,weights);
-    left_idx = xTr(feature,:)<=cut;
-    SL_x = xTr(:,left_idx);
-    SL_y = yTr(:,left_idx);
-    SL_weights = weights(:,left_idx);
-    
-    right_idx = xTr(feature,:)>cut;
-    SR_x = xTr(:,right_idx);
-    SR_y = yTr(:,right_idx);
-    SR_weights = weights(:,right_idx);
-<<<<<<< HEAD
-    
-    alloc = [ id3tree(SL_x,SL_y,maxdepth-1,SL_weights,2*current_pos) [mode(yTr);feature;cut;2*current_pos;2*current_pos+1;floor(current_pos/2);current_pos]   id3tree(SR_x,SR_y,maxdepth-1,SR_weights,2*current_pos+1)];   
-    
-end
 alloc = sortrows(alloc',7)';
 replace_indices = alloc(7,:);
+disp(T)
+disp(alloc);
 T(:,replace_indices) = alloc;
+T = T(1:6,:);
 disp(T);
 end
-=======
-    T = [id3tree(SL_x,SL_y,maxdepth-1,SL_weights,2*current_pos) [mode(yTr);feature;cut;2*current_pos;2*current_pos+1;floor(current_pos/2)] id3tree(SR_x,SR_y,maxdepth-1,SR_weights,2*current_pos+1)];   
-end
-T = sortrows(T',6)';
-%{
-current_pos = 1;
-[feature,cut,Hbest] = entropysplit(xTr,yTr,weights);
-T(:,current_pos) = [mode(yTr);feature;cut;2*current_pos;2*current_pos+1;0];
-for i=1:maxdepth
-   for position = current_pos:2*current_pos-1
-      %T(:,position) = [mode(yTr);0;0;0;0;0];
-      %if it's 2*i, get left
-      %if it's 2*i+1, get right
-      [feature,cut,Hbest] = entropysplit(xTr,yTr,weights);
-      if mod(position,2) == 0
+
+function alloc=id3treehelper(xTr,yTr,maxdepth,weights,current_pos)
+    if length(unique(yTr','rows')) == 1  || maxdepth == 0 %If all the y's are the same, or max depth reached
+        alloc = [yTr(1);0;0;0;0;floor(current_pos/2);current_pos];
+    %If all columns in xTr are the same, or max depth reached
+    elseif length(unique(xTr','rows')) == 1 || maxdepth == 0 
+        alloc = [mode(yTr);0;0;0;0;floor(current_pos/2);current_pos];
+    else
+        [feature,cut,~] = entropysplit(xTr,yTr,weights);
         left_idx = xTr(feature,:)<=cut;
         SL_x = xTr(:,left_idx);
         SL_y = yTr(:,left_idx);
         SL_weights = weights(:,left_idx);
-      end
-   end
-   current_pos = current_pos*2;
-%}
+
+        right_idx = xTr(feature,:)>cut;
+        SR_x = xTr(:,right_idx);
+        SR_y = yTr(:,right_idx);
+        SR_weights = weights(:,right_idx);
+
+        alloc = [ id3treehelper(SL_x,SL_y,maxdepth-1,SL_weights,2*current_pos),... 
+            [mode(yTr);feature;cut;2*current_pos;2*current_pos+1;floor(current_pos/2);current_pos],... 
+            id3treehelper(SR_x,SR_y,maxdepth-1,SR_weights,2*current_pos+1)];   
+    end
+    
 end
-
-
-
->>>>>>> FETCH_HEAD
